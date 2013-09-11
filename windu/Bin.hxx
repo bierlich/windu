b771a82b472ae23f17e3fcfa35899029c1e4ef33
bierlich <bierlich@gmail.com>
2013-09-11 09:20:13 +0200

    Bin

--- windu/Bin.hxx
+++ windu/Bin.hxx
@@ -6,3 +6,18 @@ ostream& operator<< (ostream& aStream, const Bin<TX> &aBin){
 	aStream << "\t\t\t<width>\n\t\t\t\t" << aBin._width << "\n\t\t\t</width>\n";
 	return aStream;
 }
+
+template<class TX>
+istream& operator>> (istream& aStream, Histogram<TX>& aHist){
+	string line;
+	while(line.find("<bin>") == string::npos){
+		getline(aStream,line);
+		if(!aStream.good()) return aStream;
+	}	
+	while(line.find("<sum_of_weights=") == string::npos){
+		getline(aStream,line);
+		if(!aStream.good()) return aStream;
+	}
+	aBin.w = to_double()
+	return aStream;
+}
